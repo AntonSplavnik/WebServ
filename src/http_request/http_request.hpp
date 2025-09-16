d4b63e5f118532ab8620c243500c47cb7d39d305
LazyPixel <antonsplavnik@gmail.com>
2025-09-16 17:19:22 +0200

    Add initial implementation of Makefile and HTTP request handling files

---
+++ src/http_request/http_request.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http_request.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antonsplavnik <antonsplavnik@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 17:16:30 by antonsplavn       #+#    #+#             */
+/*   Updated: 2025/09/16 17:17:49 by antonsplavn      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HTTP_REQUEST
+#define HTTP_REQUEST
+
+#include <iostream>
+#include <string>
+
+
+
+#endif
